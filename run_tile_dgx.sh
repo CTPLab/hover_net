44767a6b2fabde76fb1cd3dc8d9036a93ea6dbf0
musikisomorphie <wujiqing9@gmail.com>
2021-03-13 21:03:27 +0100

    use large batch size

--- run_tile_dgx.sh
+++ run_tile_dgx.sh
@@ -2,7 +2,7 @@ python run_infer.py \
 --gpu='0,1,2,3,4,5,6,7' \
 --nr_types=6 \
 --type_info_path=type_info.json \
---batch_size=64 \
+--batch_size=128 \
 --model_mode=fast \
 --model_path=/raid/jiqing/Github/hover_net/logs/1/net_epoch=4.tar \
 --nr_inference_workers=16 \
